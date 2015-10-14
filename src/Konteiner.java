89ec66b2c11c856033e0ed2a9763a746eda48c9b
kirkeN <kirke.narusk@gmail.com>
2015-10-14 20:44:26 +0300

    Tegin klassi konteinerile

---
+++ src/Konteiner.java
@@ -0,0 +1,17 @@
+/**
+ * Created by kasutaja on 14.10.2015.
+ */
+public class Konteiner {
+    private String liik;
+
+    Konteiner(String liik) {
+        this.liik = liik;
+    }
+    String getLiik () {
+        return liik;
+    }
+    void setLiik (String liik){
+        this.liik = liik;
+    }
+}
+
